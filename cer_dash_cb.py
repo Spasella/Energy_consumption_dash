94da4fcb72faceda5035adb58ce73408d845e456
Spasella <38455552+Spasella@users.noreply.github.com>
2023-01-13 16:05:16 +0100

    Update cer_dash_cb.py

--- cer_dash_cb.py
+++ cer_dash_cb.py
@@ -8,169 +8,206 @@ import plotly.graph_objects as go
 import plotly.express as px
 from dash import Dash, html, dcc
 from plotly.offline import plot
-from dash import Dash, dcc, html, Input, Output, dash_table
-from plotly.subplots import make_subplots
+from dash import Dash, dcc, html, Input, Output
 from datetime import date
 import json
-
 import requests
+import sys
+import certifi
 
-import random
-import numpy as np
-from datetime import datetime
-
-
-df_final = pd.read_csv('https://raw.githubusercontent.com/Spasella/cer/main/df_final.csv')
-
-
-#CREATE ADOBE DATASET
-regex = 'AA_M.*'
-df_AA = df_final[df_final['source'].str.match(regex)]
-
-
-
-#BING - GA - FB - GADS - - - SUMMARY
-q_summary = '''select dataset_name, _BATCH_LAST_RUN_, _COUNT_ROWS_, link from df_final where dataset_name not regexp 'CW_AA' '''
-df_summary = ps.sqldf(q_summary)
-
-
-
-
-
-
-
-#APP SPACE
-app = dash.Dash(__name__, external_stylesheets = [dbc.themes.VAPOR])
+app = dash.Dash(__name__, external_stylesheets = [dbc.themes.CYBORG])
 server = app.server
-LUXOTTICA_LOGO = "https://www.jest.it/wp-content/uploads/2018/10/Logo-Luxottica.jpg"
+
+
+
+#CONNECTION TO MONGO DB
+ca = certifi.where()
+df_hourly = pd.read_csv('https://raw.githubusercontent.com/LudovicoLanzo92/CER_DB/main/hourly_database_ID202206.csv',
+                        low_memory=False)
+
+
+#0 - NUMBER CARDS DATASET
+#---------------
+cards_query = '''
+              SELECT fascia_oraria, stabilimento, ROUND(SUM(consumi_kw_h), 2) as consumi_kw_h
+              FROM df_hourly
+              GROUP BY stabilimento, fascia_oraria
+'''
+cards_df = ps.sqldf(cards_query, locals())
+
+
+
+#1 - LINEBAR CHART DATASET
+#---------------
+linebar_query = '''
+SELECT date, anno, mese, fascia_oraria, stabilimento, ROUND(SUM(consumi_kw_h), 2) as consumi_kw_h
+FROM df_hourly
+GROUP BY date, anno, mese, fascia_oraria, stabilimento
+'''
+linebar_df = ps.sqldf(linebar_query, locals())
+
+
+
+
+PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 button = dbc.Row([
         dbc.Col(
             dbc.Button(
-                "GO TO DOMO", color="primary", className="btn btn-outline-light", n_clicks=0,
-                href='https://luxottica.domo.com/datacenter/datasources',
+                "Search", color="primary", className="ms-2", n_clicks=0,
+                href='https://dashcheatsheet.pythonanywhere.com/',
             ),
             width="auto",
         ),
-    ], className="g-0 ms-auto flex-nowrap mt-3 mt-md-0", align="right",)
-controls = dcc.DatePickerRange(
-            id="_date_picker_range_",
-            start_date=datetime(2022, 11, 18).strftime('%Y-%m-%d'),
-            end_date=datetime.today().strftime('%Y-%m-%d'),
-            style={'background-color': 'black'})
-
-
+    ], className="g-0 ms-auto flex-nowrap mt-3 mt-md-0", align="center",)
 navbar = dbc.Navbar(dbc.Container([
             html.A(
                 # Use row and col to control vertical alignment of logo / brand
                 dbc.Row(
                     [
-                        dbc.Col(html.Img(src=LUXOTTICA_LOGO, height="50px")),
-                        dbc.Col(dbc.NavbarBrand("CYBERWEEK DATA-TEAM CONTROLLER", className="ms-2")),
+                        dbc.Col(html.Img(src=PLOTLY_LOGO, height="40px")),
+                        dbc.Col(dbc.NavbarBrand("CER Profilo 2", className="ms-2")),
                     ],
                     align="center",
                     className="g-0",
                 ),
-                href="https://luxottica.domo.com/datacenter/datasources",
+                href="https://plotly.com",
                 style={"textDecoration": "none"},
             ),
             dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
             dbc.Collapse(
-                controls,
-                id="navbar-collapse_1",
-                is_open=False,
-                navbar=True,
-            ),
-            dbc.Collapse(
                 button,
-                id="navbar-collapse_2",
+                id="navbar-collapse",
                 is_open=False,
                 navbar=True,
             ),]), color="#111111", dark=True,)
+range_slider = dcc.RangeSlider(id='_RangeSlider_months_',
+                                min=1,
+                                max=12,
+                                step=None,
+                                marks={1: 'gen', 2: 'feb', 3: 'mar', 4: 'apr', 5: 'mag', 6: 'giu', 7: 'lug', 8: 'ago',
+                                       9: 'set', 10: 'ott', 11: 'nov', 12: 'dic'},
+                                value=[1, 2, 3],
+                                className='form-label'
+                                )
 
-#LAYOUT SPACE
+
+#               !!!LAYOUT SPACE!!!
 app.layout = dbc.Container([
     html.Div(id = 'parent', children = [navbar]),
     html.Nav(id='nav', className='menu'),
     html.Br(),
+    dbc.Col([
+        dbc.Row([
+            dbc.Col([
+                html.Br(),
+                dcc.Dropdown(
+                        id='_dropdown_stabilimento_',
+                        multi=True,
+                        options=[{'label': stabilimento, 'value': stabilimento} for stabilimento in radar_chart_stab_dropdown_options],
+                        value=['Frigo', 'Pozzo Conte', 'Pozzo di casa'],
+                        placeholder='Select Profile',
+                        style={'color':'blue',
+                               'background-color':'#111111',
+                               'background':'#111111'}),
+                html.Br(),
+                dcc.Dropdown(
+                    id='_dropdown_anno_',
+                    multi=True,
+                    options=[2021, 2022],
+                    value=[2021, 2022],
+                    placeholder='Select Year',
+                    style={'color': 'blue',
+                           'background-color': '#111111',
+                           'background': '#111111'}),
 
-    dbc.Row([
-        dbc.Col([
-            dbc.Card(dcc.Graph(id='linebar_chart'), class_name="card border-light mb-3")
-                ], xl= 9, xxl= 9, xs=9),
-    ]
-    ),
-    dbc.Row([
-        dbc.Col([
-            dbc.Row([
-                dbc.Col([
-                        dash_table.DataTable(
-                            data= df_summary.to_dict('records'),
-                            columns= [{"name": i, "id": i} for i in df_summary.columns],
-                            style_header={'border': 'Secondary',
-                                          'backgroundColor': 'Warning',
-                                          'color': 'black',
-                                          'fontWeight': 'bold'},
-                            style_cell={'border': '1px solid grey',
-                                        'backgroundColor': 'dark'},
-                            style_data={
-                                'color': 'Primary',
-                                'backgroundColor': 'black'
-                            },
-                            style_table={'overflowY':'scroll',
-                                         'height':500,
-                                         'width':1500,
-                                         'border': '1px Primary'},
-                            editable=True,
-                            filter_action="native",
-                            sort_action="native",
-                            sort_mode="multi",
-                            column_selectable="single",
-                            row_selectable="multi",
-                            row_deletable=False,
-                            selected_columns=[],
-                            selected_rows=[],
-                            page_action="native",
-                            page_current=0,
-                            page_size=15,
-
-                        ),
+                html.Br(),
+                dcc.Dropdown(
+                    id='_dropdown_fascia_oraria_',
+                    multi=True,
+                    options=['F1', 'F2', 'F3'],
+                    value=['F1', 'F2', 'F3'],
+                    placeholder='Select Hour Slot',
+                    style={'color': 'blue',
+                           'background-color': '#111111',
+                           'background': '#111111'}),
 
 
+            ],xl=3, lg=3, md=3, sm=3, xs=3, style={'background-color':'#111111'}),
 
-                ], xl= 6, xxl= 6, xs=6),
-                    ]),
-            html.Br(),
-
-            ],xl= 3, xxl= 3, xs=3, style={'border-color':'light'})
-
-])
+            dbc.Col([
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Card(id='summary_f1', children=[], body=True, style={'background-color': '#111111',
+                                                                                 'text-align':'center'})],
+                        xl=4, lg=4, md=4, sm=4, xs=4),
+                    dbc.Col([
+                        dbc.Card(id='summary_f2', children=[], body=True, style={'background-color': '#111111',
+                                                                                 'text-align':'center'})],
+                        xl=4, lg=4, md=4, sm=4, xs=4),
+                    dbc.Col([
+                        dbc.Card(id='summary_f3', children=[], body=True, style={'background-color': '#111111',
+                                                                                 'text-align':'center'})],
+                        xl=4, lg=4, md=4, sm=4, xs=4),
+                    dbc.Row([
+                        dbc.Col([dcc.Graph(id='linebar_chart')])
+                            ], style={'margin-top':'10px'})
+                        ])
+                    ])
+        ],),
+    ])
 
 ], fluid=True)
 
 
+#                                    !!!CALLBACK SPACE!!!
+#_____________________________________________________________________________________________
+#0- NUMBER CARDS CALLBACK
+@app.callback([Output('summary_f1', 'children'),
+               Output('summary_f2', 'children'),
+               Output('summary_f3', 'children')],
+              Input('_dropdown_stabilimento_', 'value'))
 
+def update_number_cards(value):
+    filtered_data = cards_df[cards_df["stabilimento"].isin(value)]
 
-#========================
-#    CALLBACK SPACE
-#========================
+    f1_list = ['F1']
+    f1_ds = filtered_data[filtered_data["fascia_oraria"].isin(f1_list)]
+    f1_sum = f1_ds['consumi_kw_h'].sum()
+
+    f2_list = ['F2']
+    f2_ds = filtered_data[filtered_data["fascia_oraria"].isin(f2_list)]
+    f2_sum = f2_ds['consumi_kw_h'].sum()
+
+    f3_list = ['F3']
+    f3_ds = filtered_data[filtered_data["fascia_oraria"].isin(f3_list)]
+    f3_sum = f3_ds['consumi_kw_h'].sum()
+
+    output_f1 = html.H5([f'🔵      CONSUMI F1', html.Br(),f'{f1_sum}'])
+    output_f2 = html.H5([f'🔵      CONSUMI F2', html.Br(), f'{f2_sum}'])
+    output_f3 = html.H5([f'🔵      CONSUMI F3', html.Br(), f'{f3_sum}'])
+    return output_f1, output_f2, output_f3
+
+#1- LINE CALLBACK
 @app.callback(
-    Output("linebar_chart", "figure"),
-    [Input("_date_picker_range_", "start_date"), Input("_date_picker_range_", "end_date")]
+Output('linebar_chart', 'figure'),
+    [Input('_dropdown_stabilimento_', 'value'),
+     Input('_dropdown_fascia_oraria_', 'value'),
+     Input('_dropdown_anno_', 'value')
+     #Input('_RangeSlider_months_', 'value')
+     ]
 )
+def update_linebar_chart(value_1, value_2, value_3):
+    filtered_data_1 = linebar_df[linebar_df["stabilimento"].isin(value_1)]
+    filtered_data_2 = filtered_data_1[filtered_data_1['fascia_oraria'].isin(value_2)]
+    filtered_data_3 = filtered_data_2[filtered_data_2['anno'].isin(value_3)]
+    #filtered_data_4 = filtered_data_3[filtered_data_3['mese'].isin(value_4)]
 
-def update_linebar_chart(start_date, end_date):
-    df_filter_data_1 = df_AA[(df_AA['_BATCH_LAST_RUN_DATE']>=start_date) & (df_AA['_BATCH_LAST_RUN_DATE']<=end_date)]
-    q = '''select* FROM df_filter_data_1 GROUP BY _BATCH_LAST_RUN_HOUR, dataset_name
-    '''
-    df_filter_data_2 = ps.sqldf(q)
-    print('DF FINAL',df_AA[['_BATCH_LAST_RUN_DATE', '_BATCH_LAST_RUN_HOUR','dataset_name']])
-    print('DF FILTRATO',df_filter_data_2)
-    fig = px.line(df_filter_data_2, x='_BATCH_LAST_RUN_HOUR', y='_COUNT_ROWS_',
-                 hover_data=['_COUNT_ROWS_', 'dataset_name', 'link', '_BATCH_LAST_RUN_'], color='dataset_name',
-                 #color_discrete_sequence=['#0d47a1', '#2962ff', '#e3f2fd'],
-                 custom_data=['dataset_name', 'link', '_BATCH_LAST_RUN_HOUR', '_BATCH_LAST_RUN_DATE', '_BATCH_LAST_RUN_'],
-                 labels={'_COUNT_ROWS_': 'Count Rows',
-                         'dataset_name': 'dataset_name'},
+    fig = px.bar(filtered_data_3, x='date', y='consumi_kw_h',
+                 hover_data=['consumi_kw_h', 'fascia_oraria'], color='stabilimento',
+                 color_discrete_sequence=['#0d47a1', '#2962ff', '#e3f2fd'],
+                 custom_data=['stabilimento', 'fascia_oraria', 'anno'],
+                 labels={'consumi_kw_h': 'Consumi in KW/h',
+                         'fascia_oraria': 'Fascia Oraria'},
 
                  height=400)
     fig.update_layout(template='plotly_dark')
@@ -188,11 +225,6 @@ def update_linebar_chart(start_date, end_date):
 
 
 
-
-#========================
-#RUNNING SPACE
-#========================
-#========================
+#                 !!!RUNNING!!!
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8000)
-
